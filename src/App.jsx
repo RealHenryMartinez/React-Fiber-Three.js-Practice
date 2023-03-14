6db7bcdcfc1bdf696242f62c53ecf95fcd4cc95a
RealHenryMartinez <henrymtz0390@gmail.com>
2023-03-13 22:17:08 -0700

    implemented tilt to a small bottom card and added basic HTMl component to fiber three-js cube

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,36 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import "./App.css";
 import Three from "./components/three";
+import React from "react";
+import Tilt from "react-vanilla-tilt";
 
 function App() {
   return (
-    <Canvas id="three-canvas-container" shadows>
-      <Suspense fallback={<></>}>
-        <Three />
-      </Suspense>
-    </Canvas>
+    <>
+      <Canvas id="three-canvas-container" shadows>
+        <Suspense fallback={<></>}>
+          <Three />
+        </Suspense>
+      </Canvas>
+
+      <Tilt
+        style={{
+          backgroundColor: "#fff",
+          width: 500,
+          height: 250,
+          margin: '2%'
+        }}
+        options={{ scale: 5, max: 25 }}
+      >
+        <div
+          style={{
+            padding: 25,
+          }}
+        >
+          <p>Tilt this card with mouse</p>
+        </div>
+      </Tilt>
+    </>
   );
 }
 
--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,14 +1,19 @@
 import { angleToRadians } from "../../utils/angle";
 
 import React, { useRef, useEffect } from "react";
-import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import * as THREE from 'three'
-import gsap from 'gsap'
+import * as THREE from "three";
+import gsap from "gsap";
 import { Bee } from "./Bee";
+import CardExample from "../CardExample";
 
 function index() {
-  // Code to move the camera around 
+  // Code to move the camera around
 
   // useRef -> values that persists on renders
   const orbitControlsRef = useRef(null);
@@ -33,39 +38,42 @@ function index() {
     }
   }, [orbitControlsRef.current]);
 
-   // Animation
-   const ballRef = useRef(null);
-   useEffect(() => {
-       if (!!ballRef.current) {
+  // Animation
+  const ballRef = useRef(null);
+  useEffect(() => {
+    if (!!ballRef.current) {
+      // Timeline
+      const timeline = gsap.timeline({ paused: true });
 
-           // Timeline
-           const timeline = gsap.timeline({ paused: true });
+      // x-axis motion
+      timeline.to(ballRef.current.position, {
+        x: 1,
+        duration: 4,
+        ease: "power2.out",
+      });
 
-           // x-axis motion
-           timeline.to(ballRef.current.position, {
-               x: 1,
-               duration: 4,
-               ease: "power2.out"
-           });
+      // y-axis motion
+      timeline.to(
+        ballRef.current.position,
+        {
+          y: 0.5,
+          duration: 1.5,
+          ease: "bounce.out",
+        },
+        "<"
+      );
 
-           // y-axis motion
-           timeline.to(ballRef.current.position, {
-               y: 0.5,
-               duration: 1.5,
-               ease: "bounce.out"
-           }, "<");
-
-           // Play
-           timeline.play();
-       }
-   }, [ballRef.current])
+      // Play
+      timeline.play();
+    }
+  }, [ballRef.current]);
 
   // position => [x, y, z]
   // args => arguments you pass to the component constructor to give its properties
   return (
     <>
       {/* Camera */}
-      <PerspectiveCamera makeDefault position={[0, 1, 10]} />
+      <PerspectiveCamera makeDefault position={[0, 1, 3]} />
       <OrbitControls
         ref={orbitControlsRef}
         minPolarAngle={angleToRadians(60)}
@@ -79,24 +87,29 @@ function index() {
         <meshStandardMaterial color={"#fff"} />
       </mesh> */}
 
-      <Bee />
+      <CardExample />
+      {/* <Bee /> */}
       {/* <mesh rotation={[-angleToRadians(90), 0, 0]} castShadow receiveShadow>
         <planeGeometry args={[7, 7]} />
         <meshStandardMaterial color={"#6e6a5e"} />
       </mesh> */}
 
       {/* Ambient Light */}
-      {/* <ambientLight args={["#fff", 1]} /> */}
+      {/* <ambientLight args={["#fff", 0.1]} /> */}
 
       {/* Spot Light */}
-      <spotLight args={["#ffffff", 3, 7, angleToRadians(45), 0.4]} position={[-3, 1.5, 0]} castShadow />
+      <spotLight
+        args={["#96defa", 1, 7, angleToRadians(45), 0.4]}
+        position={[-5, 1.5, 0.5]}
+        castShadow
+      />
 
       <Environment background>
-                <mesh>
-                    <sphereGeometry args={[50, 100, 100]} />
-                    <meshBasicMaterial color="#6e6a5e" side={THREE.BackSide} />
-                </mesh>
-        </Environment>
+        <mesh>
+          <sphereGeometry args={[50, 100, 100]} />
+          <meshBasicMaterial color="#233036" side={THREE.BackSide} />
+        </mesh>
+      </Environment>
     </>
   );
 }
